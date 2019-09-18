18da1665681051708c657413f04faa60ae5d22d3
Herke22 <55189463+Herke22@users.noreply.github.com>
2019-09-18 14:08:00 +0200

    Update weektaak 2.py

--- weektaak 2.py
+++ weektaak 2.py
@@ -1,9 +1,11 @@
+#dit deel vraagt om de sequentie en maakt een variabele van de letters in de sequentie.
 sequentie = input("sequentie: ")
 nucleotide_A = "A"
 nucleotide_C = "C"
 nucleotide_G = "G"
 nucleotide_T = "T"
 
+#dit deel telt de letters in de sequentie.
 aantal_A = sequentie.count(nucleotide_A)
 print ("aantal_A: ",aantal_A)
 aantal_C = sequentie.count(nucleotide_C)
@@ -13,11 +15,9 @@ print ("aantal_G: ",aantal_G)
 aantal_T = sequentie.count(nucleotide_T)
 print ("aantal_T: ",aantal_T)
 
+#dit deel rekent de resultaten uit en print die.
 ACGT = (aantal_A + aantal_C) + aantal_G + aantal_T
-
 percentage_GC = aantal_G + aantal_C / ACGT *100
-
 print ("percentage_GC", percentage_GC)
-
 print ("aantal nucleotide: ", ACGT)
 
